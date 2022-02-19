a8c5e246057d2a3a845ca640c0d4e3b2652992c5
Nitin Yadav <nitin.yadav@saavn.com>
2022-02-19 17:12:03 +0530

    Initial Commit

---
+++ SimpleTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Nitin Yadav on 19/2/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
