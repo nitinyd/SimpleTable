f66132e903a9482009e75decc49ae571f8e46166
Nitin Yadav <nitin.yadav@saavn.com>
2022-02-19 18:12:31 +0530

    Added Cell related Updates
    1. UI updates
    2. Setting data when we set cellData in cellForRowAt

--- SimpleTable/Item Display List/Custom Cell/ItemCell.swift
+++ SimpleTable/Item Display List/Custom Cell/ItemCell.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ItemCell: UITableViewCell {
 
+    //Elements
+    @IBOutlet private var primaryImageView1: UIImageView!
+    @IBOutlet private var primaryImageView2: UIImageView!
+    @IBOutlet private var secondaryImageView: UIImageView!
+    @IBOutlet private var titleLabel: UILabel!
+    
+    
+    //Properties
     var cellData: ItemModel? {
         didSet { refreshView() }
     }
@@ -23,27 +31,34 @@ class ItemCell: UITableViewCell {
 //MARK:- Helpers
 extension ItemCell {
     private func setupUI() {
-        setupFonts()
         setupColors()
         DispatchQueue.main.async { self.setupLayers()}
     }
-    private func setupFonts() {
-        
-    }
     private func setupLayers() {
-        
+        primaryImageView1.layer.cornerRadius = LayoutConstants.imageCornerRadius
+        primaryImageView2.layer.cornerRadius = LayoutConstants.imageCornerRadius
+        secondaryImageView.layer.cornerRadius = LayoutConstants.imageCornerRadius
     }
     private func setupColors() {
-        
+        self.backgroundColor = .white
+        primaryImageView1.backgroundColor = .gray
+        primaryImageView2.backgroundColor = .gray
+        secondaryImageView.backgroundColor = .gray
     }
     private func refreshView() {
         setupText()
         setupImages()
     }
     private func setupImages() {
-        
+        if let primaryImage1 = cellData?.primaryImage1 { primaryImageView1.image = UIImage(named: primaryImage1) }
+        if let primaryImage2 = cellData?.primaryImage1 { primaryImageView1.image = UIImage(named: primaryImage2) }
+        if let secondaryImage = cellData?.primaryImage1 { primaryImageView1.image = UIImage(named: secondaryImage) }
     }
     private func setupText() {
-        
+        if let title = cellData?.title {
+            titleLabel.text = title
+        } else {
+            titleLabel.isHidden = true
+        }
     }
 }
