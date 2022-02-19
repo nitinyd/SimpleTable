7c43d504ca7949fbd32c626d8f02bda79852c724
Nitin Yadav <nitin.yadav@saavn.com>
2022-02-19 17:29:13 +0530

    Initial Table view Setup
    1. Added Table view in interface builder
    2. Added table view's outlet, confirmed to delegate and datasource

--- SimpleTable/Item Display List/Controllers/ItemDisplayTableVC.swift
+++ SimpleTable/Item Display List/Controllers/ItemDisplayTableVC.swift
@@ -9,10 +9,31 @@ import UIKit
 
 class ItemDisplayTableVC: UIViewController {
 
+    //Elements
+    @IBOutlet private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
     }
+}
 
+//MARK:- TableView Delegate Methods
+extension ItemDisplayTableVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
 
+//MARK:- Helpers
+extension ItemDisplayTableVC {
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
 }
 
